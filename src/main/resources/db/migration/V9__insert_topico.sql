db8d5168dc6b93dabeff190bc6df43b9b0fbe124
Raphael Feitosa <raphaelcs2@gmail.com>
2022-06-03 16:03:00 -0300

    add migration for insert new topico

---
+++ src/main/resources/db/migration/V9__insert_topico.sql
@@ -0,0 +1,2 @@
+INSERT INTO topico(id, titulo, mensagem, data_criacao, status, curso_id, autor_id)
+    VALUES(1, 'Duvidas sobre kotlin', 'Minha função let não funciona', '2022-01-01 12:00:00', 'NAO_RESPONDIDO', 1, 1);
